5b6d89ecd705b63dec70d92e1d07e6e769c33d74
Benjamin Bengfort <benjamin@bengfort.com>
2016-11-10 17:23:29 -0500

    lightweight bootstrap template

--- bengfort/urls.py
+++ bengfort/urls.py
@@ -1,4 +1,38 @@
-from __future__ import absolute_import, unicode_literals
+# bengfort.urls
+# Application URL definition and routers.
+#
+# Author:   Benjamin Bengfort <benjamin@bengfort.com>
+# Created:  Thu Nov 10 16:50:02 2016 -0500
+#
+# Copyright (C) 2016 Bengfort.com
+# For license information, see LICENSE.txt
+#
+# ID: urls.py [] benjamin@bengfort.com $
+
+"""
+Application URL definition and routers.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+
+Examples:
+
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+
+##########################################################################
+## Imports
+##########################################################################
 
 from django.conf import settings
 from django.conf.urls import include, url
@@ -8,6 +42,14 @@ from search import views as search_views
 from wagtail.wagtailadmin import urls as wagtailadmin_urls
 from wagtail.wagtailcore import urls as wagtail_urls
 from wagtail.wagtaildocs import urls as wagtaildocs_urls
+
+##########################################################################
+## Endpoint Discovery
+##########################################################################
+
+##########################################################################
+## URL Patterns
+##########################################################################
 
 urlpatterns = [
     url(r'^django-admin/', include(admin.site.urls)),
@@ -20,6 +62,10 @@ urlpatterns = [
     url(r'', include(wagtail_urls)),
 ]
 
+
+##########################################################################
+## Debugging URLs
+##########################################################################
 
 if settings.DEBUG:
     from django.conf.urls.static import static
