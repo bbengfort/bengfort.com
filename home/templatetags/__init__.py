1c8762a29b8896f197877211a113eaabe8b23e30
Benjamin Bengfort <benjamin@bengfort.com>
2016-11-11 15:06:51 -0500

    dynamic navbar

---
+++ home/templatetags/__init__.py
@@ -0,0 +1,18 @@
+# bengfort.templatetags
+# Custom tags for the Bengfort.com project
+#
+# Author:   Benjamin Bengfort <benjamin@bengfort.com>
+# Created:  Fri Nov 11 14:47:23 2016 -0500
+#
+# Copyright (C) 2016 Bengfort.com
+# For license information, see LICENSE.txt
+#
+# ID: __init__.py [] benjamin@bengfort.com $
+
+"""
+Custom tags for the Bengfort.com project
+"""
+
+##########################################################################
+## Imports
+##########################################################################
